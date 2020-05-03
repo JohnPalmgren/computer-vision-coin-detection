28f678793c354e6846c433e12d758db38b9a5d6c
John <johnmpalmgren@gmail.com>
2020-05-03 20:41:18 +0100

    main.py finished

--- capstone_solution.py
+++ capstone_solution.py
@@ -1,30 +1,36 @@
 import numpy as np
 import cv2
 
-#Create original colour image.
-original_image = cv2.imread('coin_picture.png', 1)
-# Create greyscale image.
-img = cv2.imread('coin_picture.png', cv2.IMREAD_GRAYSCALE)
-#Add blur.
-img = cv2.GaussianBlur(img, (5,5), 0)
+def find_circles(img):
+    """Use HoughCircles to detect circles in image. 
 
-# Detect circles.
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,0.9,120,param1=50,param2=27,minRadius=60,maxRadius=120)
-# Create circle coordinates as integers
-circles_int = np.uint16(np.around(circles))
+    return co-ordinates of the circle on the image and the size of the radius as an integer value 
+    """
 
-count = 1
-for i in (circles[0]):
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,0.9,120,param1=50,param2=27,minRadius=60,maxRadius=120)
+    return np.uint16(np.around(circles))
+
+def draw_circles(circles,image):
+    """Draws circles onto an image.
+
+    circles: list containing xy co-ordinates and the size of radius.
+    image: image on which to draw circles.
+    """
+    count = 1
+    for i in (circles[0]):
     
-    # Draw the outer circle. 
-    cv2.circle(original_image, (i[0],i[1]), i[2], (255,0,0), 4)
-    # Draw centre of circle.
-    cv2.circle(original_image, (i[0],i[1]), 2, (0,0,255), 2)
-    # Add text counting circles.
-    #cv2.putText(original_image, str(count), (i[0],i[1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0) ,2)
-    count+=1
+        # Draw the outer circle. 
+        cv2.circle(image, (i[0],i[1]), i[2], (255,0,0), 4)
+        # Draw centre of circle.
+        cv2.circle(image, (i[0],i[1]), 2, (0,0,255), 2)
+        count+=1
 
 def get_radius(circles):
+    """Return a list of radius values
+
+    Circles: a list of co-ordinates and radius of circles in image.
+    """
+
     radius = []
     for coords in circles[0,:]:
         radius.append (coords[2])
@@ -44,20 +50,15 @@ def get_brightness(img, circles, size):
     for coords in circles[0,:]:
         #get average no of pixles (brightness)
         av_brightness = np.mean(img [coords[1] - size : coords[1] + size, coords[0] - size : coords[0] + size])
-        #print (img[coords[1] - size : coords[1] + size, coords[0] - size : coords[0] + size])
         av_value.append(av_brightness)
     return av_value
 
-radii = get_radius(circles_int)
-
-bright_values = get_brightness(img,circles_int,20)
-
-#print ('radii',radii,'\nbright value',bright_values)
-
 def sort_coins(radii, bright_values):
+    """Return value of coins based on their brightness and radius"""
+
     values = []
     
-    for r,b in zip(radii,bright_values):
+    for r,b in zip(radii, bright_values):
         if r > 110 and b > 170:
             values.append(10)
         elif r < 110 and b > 170:
@@ -68,21 +69,24 @@ def sort_coins(radii, bright_values):
             values.append(1)
     return values 
 
-def add_results_to_image(coin_values,circles):
+def add_results_to_image(coin_values, circles, image):
+    """Display the total value and the value of each coin on the image
+    
+    coin_values: list of values of coins
+    circles: circle co-ordinates
+    image: image on which to display results
+    """
+
     # Add text to show total value of coins
-    cv2.putText(original_image, f'Estimated total value {str(sum(coin_values))}p', (200,100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0) ,2)
+    cv2.putText(image, f'Estimated total value {str(sum(coin_values))}p', (200,100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0) ,2)
     # Add text to each coin with the coin value
     count=0
     for coords in circles[0,:]:
-        cv2.putText(original_image, str(coin_values[count]), (coords[0],coords[1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0) ,2)
+        cv2.putText(image, str(coin_values[count]), (coords[0],coords[1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0) ,2)
         count+=1
     
-coin_values = sort_coins(radii, bright_values)
+def show_image(image):
 
-add_results_to_image(coin_values,circles)
-
-
-# Show image.
-cv2.imshow('image', original_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import cv2
 import capstone_solution as cs
 
-radii = cs.get_radius(circles_int)
+#Create original colour image.
+original_image = cv2.imread('coin_picture.png', 1)
+# Create greyscale image and add blur.
+img = cv2.imread('coin_picture.png', cv2.IMREAD_GRAYSCALE)
+img = cv2.GaussianBlur(img, (5,5), 0)
 
-bright_values = cs.get_brightness(img,circles_int,20)
+circles = cs.find_circles(img)
+
+radii = cs.get_radius(circles)
+
+bright_values = cs.get_brightness(img,circles,20)
+
+coin_values = cs.sort_coins(radii, bright_values)
+
+cs.draw_circles(circles, original_image)
+
+cs.add_results_to_image(coin_values, circles, original_image)
+
+cs.show_image(original_image)
 
