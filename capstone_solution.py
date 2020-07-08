0acdd67cc44ddba838ef7a554337b5d5516e11a1
John <johnmpalmgren@gmail.com>
2020-07-08 06:46:28 +0100

    intergrated 50p detection into program

--- capstone_solution.py
+++ capstone_solution.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+  
 def find_circles(img):
     """Use HoughCircles to detect circles in image. 
 
@@ -21,9 +21,46 @@ def draw_circles(circles,image):
     
         # Draw the outer circle. 
         cv2.circle(image, (i[0],i[1]), i[2], (255,0,0), 4)
-        # Draw centre of circle.
-        cv2.circle(image, (i[0],i[1]), 2, (0,0,255), 2)
+
         count+=1
+
+def find_50p(img):
+    """Detect contours and area of shapes in the image and filters to detect 50p.
+
+    Return contours matching the area of 50p and number of 50 pence detected.
+    """
+
+    imgcanny = cv2.Canny(img,16,14)
+    kernel = np.ones((5, 5))
+    imgdil = cv2.dilate(imgcanny, kernel, iterations=1)
+
+    contours, hierarchy = cv2.findContours(imgdil, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    fifty = []
+    count = 0
+    
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 60000 and area < 70000:
+            fifty.append(cnt)
+            count += 1
+
+
+    return fifty, count
+
+
+def draw_50p(contours, image):
+    """Draw contours around 50p in image
+    
+    contours: co-ordinates for contours around detected 50 pence
+    image: image on which to draw contours
+    """
+
+    for cnt in contours:
+        cv2.drawContours(image, cnt, -1, (255, 0, 0), 4)
+        cv2.putText(image, "50",  (835,393), cv2.FONT_HERSHEY_SIMPLEX, 2, #get position from coords
+                    (0, 0, 0), 2)
+
 
 def get_radius(circles):
     """Return a list of radius values
@@ -67,25 +104,29 @@ def sort_coins(radii, bright_values):
             values.append(2)
         elif r < 110 and b < 170:
             values.append(1)
+
+
     return values 
 
-def add_results_to_image(coin_values, circles, image):
+def add_results_to_image(circle_coin_values, fifty_pence_values, circles, image):
     """Display the total value and the value of each coin on the image
     
-    coin_values: list of values of coins
+    circle_coin_values: list of values of coins excluding 50p
+    fifty_pence_vlaues: values of 50 pence
     circles: circle co-ordinates
     image: image on which to display results
     """
 
     # Add text to show total value of coins
-    cv2.putText(image, f'Estimated total value {str(sum(coin_values))}p', (200,100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0) ,2)
+    cv2.putText(image, f'Estimated total value {str(sum(circle_coin_values)+fifty_pence_values)}p', (200,100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0) ,2)
     # Add text to each coin with the coin value
     count=0
     for coords in circles[0,:]:
-        cv2.putText(image, str(coin_values[count]), (coords[0],coords[1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0) ,2)
+        cv2.putText(image, str(circle_coin_values[count]), (coords[0],coords[1]), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,0) ,2)
         count+=1
     
 def show_image(image):
+    """Display image"""
 
     cv2.imshow('image', image)
     cv2.waitKey(0)
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import cv2
 import capstone_solution as cs
 
-#Create original colour image.
-original_image = cv2.imread('coin_picture.png', 1)
-# Create greyscale image and add blur.
-img = cv2.imread('coin_picture.png', cv2.IMREAD_GRAYSCALE)
-img = cv2.GaussianBlur(img, (5,5), 0)
+if __name__ == '__main__':
 
-circles = cs.find_circles(img)
+    #Create original colour image.
+    original_image = cv2.imread('coin_picture.png', 1)
+    # Create greyscale image and add blur.
+    img = cv2.imread('coin_picture.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.GaussianBlur(img, (5,5), 0)
 
-radii = cs.get_radius(circles)
+    circles = cs.find_circles(img)
 
-bright_values = cs.get_brightness(img,circles,20)
+    radii = cs.get_radius(circles)
 
-coin_values = cs.sort_coins(radii, bright_values)
+    bright_values = cs.get_brightness(img,circles,20)
 
-cs.draw_circles(circles, original_image)
+    fifty_contours = cs.find_50p(img)[0] # Contours of 50p coins
 
-cs.add_results_to_image(coin_values, circles, original_image)
+    fifty_coins_value = cs.find_50p(img)[1]*50 # Value of of 50p coins in image. 
 
-cs.show_image(original_image)
+    coin_values = cs.sort_coins(radii, bright_values)
 
+    cs.draw_circles(circles, original_image)
+
+    cs.draw_50p(fifty_contours, original_image)
+
+    cs.add_results_to_image(coin_values, fifty_coins_value, circles, original_image)
+
+    cs.show_image(original_image)
